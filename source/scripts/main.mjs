095b45d164ea20f83a39f226f824a9a392afbef9
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-09-11 02:20:00 -0500

    refactor: rename parameter from 'node' to 'root' for consistency in trait function

--- source/scripts/main.mjs
+++ source/scripts/main.mjs
@@ -1,17 +1,17 @@
 import { trait } from './trait.mjs';
 
-trait((node, templates) => {
+trait((root, templates) => {
   'use strict';
 
-  console.debug('trait-clock', node, templates);
+  console.debug('trait-clock', root, templates);
 
   let date = new Date();
 
-  node.textContent = date.toLocaleTimeString();
+  root.textContent = date.toLocaleTimeString();
 
   setInterval(() => {
     date = new Date();
-    node.textContent = date.toLocaleTimeString();
+    root.textContent = date.toLocaleTimeString();
   }, 1000);
 
 }, 'trait-clock', 'trait-clock-template');
