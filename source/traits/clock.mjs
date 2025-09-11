5c935d5be5533d8408a0e460a04a361880895a82
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-09-11 01:58:39 -0500

    refactor: enhance clock trait by adding callback parameter and simplifying template handling

--- source/traits/clock.mjs
+++ source/traits/clock.mjs
@@ -4,18 +4,29 @@
    * @param {NodeList} nodeList Nodes with the trait-clock attribute
    * @param {NodeList} templateNodeList Nodes with the trait-clock-template attribute
    */
-  (window, nodeList, templateNodeList) => {
-    'use strict';
+  (window, nodeList, templateNodeList, callback) => {
+    const templates = {};
 
-    /** @type {Record<string, HTMLTemplateElement>} */
-    const templates = Object.freeze(
-      Array.from(templateNodeList)
-        .filter(template => template.hasAttribute('id'))
-        .reduce((acc, tem) => (acc[tem.getAttribute('id')] = tem) && acc, {})
-    );
+    for (const tem of templateNodeList)
+      if (tem.hasAttribute('id'))
+        templates[tem.getAttribute('id')] = tem;
 
-    /* Trait callback */
-    nodeList.forEach((node) => {
+    Object.freeze(templates);
+
+    for (const node of nodeList) callback(window, node, templates);
+  })(
+    window,
+    document.querySelectorAll('[trait-clock]'),
+    document.querySelectorAll('[trait-clock-template]'),
+    /**
+     *
+     * @param {Window} window
+     * @param {Node} node
+     * @param {Record<string, HTMLTemplateElement>} templates
+     */
+    (window, node, templates) => {
+      'use strict';
+      console.debug('trait-clock', window, node, templates);
       let date = new Date();
 
       node.textContent = date.toLocaleTimeString();
@@ -24,10 +35,5 @@
         date = new Date();
         node.textContent = date.toLocaleTimeString();
       }, 1000);
-    });
-
-  })(
-    window,
-    document.querySelectorAll('[trait-clock]'),
-    document.querySelectorAll('[trait-clock-template]'),
+    }
   );
