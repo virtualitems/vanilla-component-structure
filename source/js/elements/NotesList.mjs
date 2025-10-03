eb123e5be9f23d17200ba36ecf3dec8a97995dee
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-10-03 04:12:57 -0500

    feat: implement DeleteNoteHandler for handling note deletion in NotesList

--- source/js/elements/NotesList.mjs
+++ source/js/elements/NotesList.mjs
@@ -1,4 +1,12 @@
 import { BaseCustomElement } from '../elements.mjs';
+import { EventHandler } from '../events.mjs';
+
+class DeleteNoteHandler extends EventHandler {
+  handleEvent(event) {
+    event.stopPropagation();
+    this.host.remove();
+  }
+}
 
 export class NotesList extends BaseCustomElement {
 
@@ -54,7 +62,7 @@ export class NotesList extends BaseCustomElement {
     item.textContent = text;
 
     const button = item.shadowRoot.querySelector('action-button');
-    button?.addEventListener('click', () => item.remove());
+    button?.addEventListener('click', new DeleteNoteHandler(item));
 
     listElement.appendChild(item);
   }
