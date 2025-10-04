c6c2d25f8d3db3a6245a778a85ec7a8d616ff412
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-10-04 02:05:25 -0500

    refactor: export $table and $datatable for improved modularity

--- source/js/jquery/colors-table.mjs
+++ source/js/jquery/colors-table.mjs
@@ -5,7 +5,7 @@ import jq from 'https://esm.sh/jquery@3.7.1';
 import { lang } from './html.mjs';
 
 
-const $table = jq('#colors-table');
+export const $table = jq('#colors-table');
 
 const language = {
   url: $table.data(`lang-url-${lang}`),
@@ -50,22 +50,73 @@ async function ajax(tabledata, done, settings) {
 };
 
 const columns = [
-  { data: 'id', title: 'ID' },
-  { data: 'name', title: 'Name' },
-  { data: 'year', title: 'Year' },
-  { data: 'color', title: 'Color' },
-  { data: 'pantone_value', title: 'Pantone Value' },
+  {
+    data: 'id',
+    title: 'ID',
+    render: function (data, type, row, meta) {
+      if (type === 'display') {
+        return `<i>${data}</i>`;
+      }
+
+      return data;
+    }
+  },
+  {
+    data: 'name',
+    title: 'Name',
+    render: function (data, type, row, meta) {
+      if (type === 'display') {
+        return `<i>${data}</i>`;
+      }
+
+      return data;
+    }
+  },
+  {
+    data: 'year',
+    title: 'Year',
+    render: function (data, type, row, meta) {
+      if (type === 'display') {
+        return `<i>${data}</i>`;
+      }
+
+      return data;
+    }
+  },
+  {
+    data: 'color',
+    title: 'Color',
+    render: function (data, type, row, meta) {
+      if (type === 'display') {
+        return `<i>${data}</i>`;
+      }
+
+      return data;
+    }
+  },
+  {
+    data: 'pantone_value',
+    title: 'Pantone Value',
+    render: function (data, type, row, meta) {
+      if (type === 'display') {
+        return `<i>${data}</i>`;
+      }
+
+      return data;
+    }
+  },
 ];
 
-$table.DataTable({
-  serverSide: true,
-  processing: true,
-  paging: true,
-  searching: true,
-  ordering: true,
-  language,
-  pageLength: 2,
-  lengthMenu: [2, 4, 6, 8, 10],
-  ajax,
-  columns,
-});
+export const $datatable = $table
+  .DataTable({
+    serverSide: true,
+    processing: true,
+    paging: true,
+    searching: true,
+    ordering: true,
+    language,
+    pageLength: 2,
+    lengthMenu: [2, 4, 6, 8, 10],
+    ajax,
+    columns,
+  });
