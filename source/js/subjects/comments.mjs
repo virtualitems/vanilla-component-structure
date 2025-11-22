cc5595ffd3fc103c8ca3a89e68f78e5223f5af16
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-11-22 01:29:39 -0500

    feat: add CommentsSubject and PostsSubject classes for observable post and comment handling

---
+++ source/js/subjects/comments.mjs
@@ -0,0 +1,22 @@
+import { Subject } from '../shared/subjects.mjs';
+
+/**
+ * @description Observable subject for post comments
+ */
+export class CommentsSubject extends Subject
+{
+  // attributes
+
+  // static attributes
+
+  // Constructor
+
+  // getters/setters
+
+  // static getters/setters
+
+  // methods
+
+  // static methods
+
+} //:: class
---
+++ source/js/subjects/posts.mjs
@@ -0,0 +1,22 @@
+import { Subject } from '../shared/subjects.mjs';
+
+/**
+ * @description Observable subject for posts
+ */
+export class PostsSubject extends Subject
+{
+  // attributes
+
+  // static attributes
+
+  // Constructor
+
+  // getters/setters
+
+  // static getters/setters
+
+  // methods
+
+  // static methods
+
+} //:: class
