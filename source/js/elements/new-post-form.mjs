9d714033b1689065a16ae1ab90e80a6f6188c7d3
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-11-22 03:01:50 -0500

    refactor: standardize form data handling in post creation and submission

--- source/js/elements/new-post-form.mjs
+++ source/js/elements/new-post-form.mjs
@@ -6,11 +6,13 @@ rootElement.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const formData = new FormData(rootElement);
-  const plainData = Object.fromEntries(formData);
 
-  const payload = { detail: plainData };
+  const payload = { detail: formData };
+
   const type = PostsSubject.events.CREATE_POST;
+
   const customEvent = new CustomEvent(type, payload);
+
   PostsSubject.dispatchEvent(customEvent);
 
   rootElement.reset();
