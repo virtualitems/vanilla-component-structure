c61ef4c312366f0c51d86084c56725ebbe0981e1
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-11-22 15:53:21 -0500

    refactor: clear out unused translation keys in language resources

--- source/js/shared/languages.mjs
+++ source/js/shared/languages.mjs
@@ -6,20 +6,10 @@ i18n.init({
   lng: 'es',
   resources: {
     es: {
-      translation: {
-        'app.title': 'Aplicación de Notas',
-        'form.placeholder': 'Nueva nota',
-        'form.button': 'Agregar',
-        'note.delete': '×'
-      }
+      translation: { }
     },
     en: {
-      translation: {
-        'app.title': 'Notes App',
-        'form.placeholder': 'New note',
-        'form.button': 'Add',
-        'note.delete': '×'
-      }
+      translation: { }
     }
   }
 });
