cd0ec666e649c808cd554d90f759f3bc6eb3f33f
Alejandro CR <virtualitemsuniverse@gmail.com>
2025-11-22 00:01:36 -0500

    feat: implement Comment component to display post comments

---
+++ source/js/components/comment.mjs
@@ -0,0 +1,65 @@
+import { BaseCustomElement } from '../shared/elements.mjs';
+
+/**
+ * @description Represents a comment on a post
+ */
+export class Comment extends BaseCustomElement {
+
+  // attributes
+
+  // static attributes
+
+  // Constructor
+
+  // getters/setters
+
+  // static getters/setters
+
+  // methods
+
+  connectedCallback() {
+    this.replaceTextContent('author', this.getAttribute('data-author'));
+    this.replaceTextContent('content', this.getAttribute('data-content'));
+    this.replaceTextContent('date', this.getAttribute('data-date'));
+  }
+
+  // static methods
+
+} //:: class
+
+Comment.html = `
+  <div id="author"></div>
+  <div id="date"></div>
+  <div id="content"></div>
+`;
+
+Comment.css = `
+  :host {
+    background-color: #f9f9f9;
+    border-radius: 4px;
+    padding: 0.75rem;
+    margin-bottom: 0.5rem;
+    display: block;
+  }
+
+  #author {
+    color: var(--primary-color);
+    font-size: 0.85rem;
+    font-weight: 500;
+    margin-bottom: 0.25rem;
+  }
+
+  #date {
+    color: #999;
+    font-size: 0.75rem;
+    margin-bottom: 0.25rem;
+  }
+
+  #content {
+    color: #555;
+    font-size: 0.9rem;
+    line-height: 1.5;
+  }
+`;
+
+Comment.tagName = 'app-comment';
